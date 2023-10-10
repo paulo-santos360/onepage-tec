9ceda10990fae13034eae53ef05eef1cddfd7dbc
Paulo Santos <102436341+paulo-santos360@users.noreply.github.com>
2023-10-10 15:12:06 -0300

    Atualizar

--- js/app.js
+++ js/app.js
@@ -20,3 +20,20 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+// Seta Topo
+const rolagem = ()=> {
+  const html = document.documentElement
+  const seta = document.getElementById('go-top')
+
+  /* Se a rolagem for maior que 550, a seta aparece 
+  abaixo de 550 esconde */
+  if (html.scrollTop > 550 ) {
+    seta.style.display = 'block'
+  } else {
+    seta.style.display = 'none'
+  }
+}
+
+//window.addEventListener('scroll', rolagem)
+window.onscroll = ()=> rolagem()//evento somente para elem q existem no html
